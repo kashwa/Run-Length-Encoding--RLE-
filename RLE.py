6349232bf1d82eda2665303eb614f934a81d5637
Aabed Kashwa <kashwa80@gmail.com>
2018-01-20 17:13:34 +0200

    Commenting RLE.py to clarify

--- RLE.py
+++ RLE.py
@@ -2,12 +2,13 @@ from collections import Counter
 from tkinter import *
 from tkinter import messagebox
 
+# lst is a variable used to mainly contain the message alphabets.
 lst = []
 def encode(enc):
     counts = Counter(enc)
     temp = []
 
-    for i in counts:
+    for i in counts: # Cutting alphabets from the msg and appending it into [lst].
         temp += (str(counts[i]) + i)
         y = (i, counts[i])
         lst.append(y)
@@ -17,7 +18,7 @@ def encode(enc):
 
 
 
-
+# Decoding the input MSG.
 def decode(x):
     q = ""
     for character, count in x:
@@ -30,7 +31,8 @@ def printmethods():
     messagebox.showinfo("Decompresses", decomp)
 
 
-
+''' Ui Components,
+    here i used simple user interface to preview compilation of code. '''
 root = Tk()
 root.title('RLE')
 mLabel=Label(root,text="RLE Compression").grid(columnspan=2)
